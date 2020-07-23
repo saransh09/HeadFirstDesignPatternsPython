4ca75a9e281934f860e004ac4ef0b0ad399e0a2a
Saransh Agarwal <saransh.agarwal@gmail.com>
2020-07-23 23:02:06 +0530

    [1_strategy] completed the first chapter

--- 1_strategy/ex_3.py
+++ 1_strategy/ex_3.py
@@ -14,9 +14,16 @@ class Charachter(abc.ABC):
 
 
 	def set_weapon_behavior(self, weapon='knife'):
-		pass
-
-
+		if weapon=='knife':
+			self.weapon_type = KnifeBehavior()
+		elif weapon=='bowandarrow':
+			self.weapon_type = BowAndArrowBehavior()
+		elif weapon=='axe':
+			self.weapon_type = AxeBehavior()
+		elif weapon=='sword':
+			self.weapon_type = SwordBehavior()
+		else:
+			print("Select either (knife/bowandarrow/axe/sword)")
 
 
 
@@ -73,3 +80,23 @@ class SwordBehavior(Weapon):
 		print("Sword through the opponents body!")
 
 ########################################################################################
+
+
+class Queen(Charachter):
+
+	def __init__(self):
+		self.set_weapon_behavior('bowandarrow')
+
+	def weapon_action(self):
+		self.weapon_type.weapon_behavior()
+
+	def info():
+		print("This is the queen")
+
+
+
+
+if __name__ == '__main__':
+
+	q = Queen()
+	q.weapon_action()
